85c3fbb13bdc226d1576e0185203a5bef0c6962a
LinhChi2511 <chidao2511@gmail.com>
2025-03-24 18:20:02 +0700

    test

---
+++ src/test/java/codegym/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
